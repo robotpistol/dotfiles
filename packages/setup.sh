4ae22ad0aa5573cbb2732f2be4737c8af82273c4
Allen Kerr <alkerr@gmail.com>
2019-01-22 23:26:37 -0800

    Delete a bunch fo stuff i won't use

--- packages/setup.sh
+++ packages/setup.sh
@@ -14,15 +14,15 @@ brew bundle
 success "Finished installing Brewfile packages."
 
 find * -name "*.list" | while read fn; do
-    cmd="${fn%.*}"
-    set -- $cmd
-    info "Installing $1 packages..."
-    while read package; do
-        if [[ $package == $COMMENT ]];
-        then continue
-        fi
-        substep_info "Installing $package..."
-        $cmd $package
-    done < "$fn"
-    success "Finished installing $1 packages."
+  cmd="${fn%.*}"
+  set -- $cmd
+  info "Installing $1 packages..."
+  while read package; do
+    if [[ $package == $COMMENT ]];
+    then continue
+    fi
+    substep_info "Installing $package..."
+    $cmd $package
+  done < "$fn"
+  success "Finished installing $1 packages."
 done
