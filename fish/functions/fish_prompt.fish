6562572e16435ef41c6c6c0c5a52821fc93cd162
Allen Kerr <alkerr@gmail.com>
2019-11-13 08:52:07 -0800

    use bobthefish

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,91 +1,33 @@
-function fish_prompt --description 'Write out the prompt'
-	set -l last_status $status
+function fish_prompt
+	# Store the exit code of the last command
+	set -g sf_exit_code $status
+	set -g SPACEFISH_VERSION 2.7.0
 
-	# Just calculate this once, to save a few cycles when displaying the prompt
-	if not set -q __fish_prompt_hostname
-		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+	# ------------------------------------------------------------------------------
+	# Configuration
+	# ------------------------------------------------------------------------------
+
+	__sf_util_set_default SPACEFISH_PROMPT_ADD_NEWLINE true
+	__sf_util_set_default SPACEFISH_PROMPT_FIRST_PREFIX_SHOW false
+	__sf_util_set_default SPACEFISH_PROMPT_PREFIXES_SHOW true
+	__sf_util_set_default SPACEFISH_PROMPT_SUFFIXES_SHOW true
+	__sf_util_set_default SPACEFISH_PROMPT_DEFAULT_PREFIX "via "
+	__sf_util_set_default SPACEFISH_PROMPT_DEFAULT_SUFFIX " "
+	__sf_util_set_default SPACEFISH_PROMPT_ORDER time user dir host git package node ruby golang php rust haskell julia elixir docker aws venv conda pyenv dotnet kubecontext exec_time line_sep battery vi_mode jobs exit_code char
+
+	# ------------------------------------------------------------------------------
+	# Sections
+	# ------------------------------------------------------------------------------
+
+	# Keep track of whether the prompt has already been opened
+	set -g sf_prompt_opened $SPACEFISH_PROMPT_FIRST_PREFIX_SHOW
+
+	if test "$SPACEFISH_PROMPT_ADD_NEWLINE" = "true"
+		echo
 	end
 
-	set -l normal (set_color normal)
-
-	# Hack; fish_config only copies the fish_prompt function (see #736)
-	if not set -q -g __fish_classic_git_functions_defined
-		set -g __fish_classic_git_functions_defined
-
-		function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
-
-		function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
-
-		function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
-
-		function __fish_repaint_bind_mode --on-variable fish_key_bindings --description "Event handler; repaint when fish_key_bindings changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
-
-		# initialize our new variables
-		if not set -q __fish_classic_git_prompt_initialized
-			set -qU fish_color_user; or set -U fish_color_user -o green
-			set -qU fish_color_host; or set -U fish_color_host -o cyan
-			set -qU fish_color_status; or set -U fish_color_status red
-			set -U __fish_classic_git_prompt_initialized
-		end
+	for i in $SPACEFISH_PROMPT_ORDER
+		eval __sf_section_$i
 	end
-
-	set -l color_cwd
-	set -l prefix
-	switch $USER
-	case root toor
-		if set -q fish_color_cwd_root
-			set color_cwd $fish_color_cwd_root
-		else
-			set color_cwd $fish_color_cwd
-		end
-		set suffix '#'
-	case '*'
-		set color_cwd $fish_color_cwd
-		set suffix '>'
-	end
-
-	set -l prompt_status
-	if test $last_status -ne 0
-		set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
-	end
-
-	set -l mode_str
-	switch "$fish_key_bindings"
-	case '*_vi_*' '*_vi'
-		# possibly fish_vi_key_bindings, or custom key bindings
-		# that includes the name "vi"
-		set mode_str (
-			echo -n " "
-			switch $fish_bind_mode
-			case default
-				set_color --bold --background red white
-				echo -n "[N]"
-			case insert
-				set_color --bold green
-				echo -n "[I]"
-			case visual
-				set_color --bold magenta
-				echo -n "[V]"
-			end
-			set_color normal
-		)
-	end
-
-	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "> "
+	set_color normal
 end
